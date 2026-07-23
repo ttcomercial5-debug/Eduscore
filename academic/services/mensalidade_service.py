f7094b2b66c7604b044e8d32e4fdddb2b568d86b
Ivo Cardoso Avelino <icardosoavelino13@gmail.com>
2026-07-23 01:25:41 +0100

    update_133

--- academic/services/mensalidade_service.py
+++ academic/services/mensalidade_service.py
@@ -14,18 +14,16 @@ from academic.models import ConfiguracaoMensalidade
 
 MESES = [
 
+    ("09","Setembro"),
+    ("10","Outubro"),
+    ("11","Novembro"),
+    ("12","Dezembro"),
     ("01","Janeiro"),
     ("02","Fevereiro"),
     ("03","Março"),
     ("04","Abril"),
     ("05","Maio"),
     ("06","Junho"),
-    ("07","Julho"),
-    ("08","Agosto"),
-    ("09","Setembro"),
-    ("10","Outubro"),
-    ("11","Novembro"),
-    ("12","Dezembro"),
 
 ]
 
